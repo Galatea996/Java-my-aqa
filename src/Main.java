e861d2c6b3451a18e88f19247dc6be30eb715df6
Marisha <galatea996@gmail.com>
2024-03-12 12:52:35 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String smile = "\uD83D\uDE06";
+        String sm = "\uD83D\uDD0A";
+        System.out.println(smile);
+        System.out.println(sm);
+    }
+}
